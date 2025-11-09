3a11f50fe792b4b0a7c52d53f6e0d1fc386ba32f
Mehmetcan Bozkuş <mtcnbzks@gmail.com>
2025-11-09 22:24:42 +0300

    Initial Commit

---
+++ PentaCapture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PentaCapture
+//
+//  Created by Mehmetcan Bozkuş on 9.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PentaCapture/PentaCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentaCaptureApp.swift
+//  PentaCapture
+//
+//  Created by Mehmetcan Bozkuş on 9.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PentaCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
